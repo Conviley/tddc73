26fbb3f3f22ae59a4a462c6391f2b6d20a361b38
Tjelvar Guo <tjegu689@student.liu.se>
2018-12-05 20:40:57 +0100

    remove unused imports

--- lab11/app/src/main/java/com/example/tjelvarguo/lab1/lab3/Lab3.java
+++ lab11/app/src/main/java/com/example/tjelvarguo/lab1/lab3/Lab3.java
@@ -2,27 +2,8 @@ package com.example.tjelvarguo.lab1.lab3;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.tjelvarguo.lab1.R;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.util.ArrayList;
 
 public class Lab3 extends Activity {
 
--- lab11/app/src/main/java/com/example/tjelvarguo/lab1/lab3/NameRow.java
+++ lab11/app/src/main/java/com/example/tjelvarguo/lab1/lab3/NameRow.java
@@ -4,10 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 /**
